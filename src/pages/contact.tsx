542f591bc8c3aa53d791920f51b23460a58d76f5
JakeBartoletta <Jake.Bartoletta@fortisureit.com>
2024-01-10 09:07:21 -0500

    added "portfolio" and "contact" pages and updated home page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,15 +1,15 @@
 import Head from 'next/head'
 import React from 'react';
 
-export default function HomePage() {
+export default function ContactPage() {
     return (
     <React.Fragment>
         <Head>
-            <title>Home</title>
+            <title>Contact</title>
         </Head>
 
         <div>
-            Index Page
+            Contact Page
         </div>
     </React.Fragment>
     )
